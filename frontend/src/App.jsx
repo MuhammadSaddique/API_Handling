b5d5bc5ce1f8a6498ec1a936afa36ca08c77dbcd
saddique <saddique@gmil.com>
2024-06-03 17:40:40 +0500

    modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,46 @@ import axios from 'axios'
 import { useEffect } from 'react';
 
 function App() {
-  const [products , setProducts] = useState([]);
 
-  useEffect(()=>{
-    (async()=>{
-     const response = await axios.get('/')
-     console.log(response.data);
-     setProducts(response.data);
-    })()
-  },[]);
+//  const [products,error,loading] =  customReactQuery('/');
 
+const [products , setProducts] = useState([]);
+const [error,SetError] = useState(false);
+const [loading , setLoading] = useState(false);
+const [search , setSearch] = useEffect('API');
+
+useEffect(()=>{
+  (async()=>{
+   try {
+     setLoading(true);
+    SetError(false)
+    const response = await axios.get('/?search='+ search)
+    console.log(response.data);
+    setProducts(response.data);
+    setLoading(false)
+   } catch (error) {
+    SetError(true)
+    setLoading(false)
+   }
+  })()
+},[search]);
+
+
+
+  // if(error){
+  //   return <h1>Something went wrongs</h1>
+  // }
+  // if(loading){
+  //   return <h1>Loading......</h1>
+// }
 
   return (
     <>
      <h1>Calling of API in Reacts</h1>
+     <input type="text" placeholder='Search' value={search}
+     onChange={(e)=>setSearch(e.target.value)} />
+     {loading && (<h1>Loading...</h1>)}
+     {error && (<h1>Something went wrongs...</h1>)}
      <h2> Number of Products : {products.length}</h2>
 
          </>
@@ -27,3 +53,9 @@ function App() {
 }
 
 export default App
+
+const customReactQuery = (urlPath)=>{
+ 
+  // return [products,error,loading]; 
+}
+
